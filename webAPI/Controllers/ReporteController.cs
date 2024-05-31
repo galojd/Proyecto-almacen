dbe440c58744215477199574c2a261469307456a
galo <marc_tony22@outlook.com>
2024-05-31 00:35:03 -0500

    reporte excel

--- webAPI/Controllers/ReporteController.cs
+++ webAPI/Controllers/ReporteController.cs
@@ -23,12 +23,33 @@ namespace webAPI.Controllers
         return Ok(resultado);
         }
 
+        //Aqui esta el reporte de los productos
+        //  http://localhost:5235/api/Reporte
+        [HttpGet("producto-excel")]
+        public async Task<ActionResult> GetReporteProductoExcel()
+        {
+            var resultado = await Mediator.Send(new ReporteProductosExcel.ListaReporte());
+            if (resultado == null || !resultado.Any())
+            {
+                return NotFound(); // o puedes retornar NotFound() si prefieres
+            }
+
+            return File(resultado, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Reporte de producto.xlsx");
+        }
+
         //este es el reporte de stock bajo
         // http://localhost:5235/api/Reporte/StockBajo
         [HttpGet("StockBajo")]
         public async Task<ActionResult<List<ReporteStockBajo>>> GetStock()
         {
             return await Mediator.Send(new ReportestockBajos.ListaReportebajo());
+        }
+
+        [HttpGet("Stock-bajo-excel")]
+        public async Task<ActionResult> GetStockbajo()
+        {
+            var resultado = await Mediator.Send(new Reportestockbajoexcel.ListaReporte());
+            return File(resultado, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Reporte de stock bajo.xlsx");
         }
 
 
